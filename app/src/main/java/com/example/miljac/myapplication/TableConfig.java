70ccb49eb8c25e06dae59d9ae7b39d0d28f1fa62
milicajac <miljenko.stojicic@gmail.com>
2017-09-28 18:04:03 +0200

    ljepse je sad kaj ne

--- app/src/main/java/com/example/miljac/myapplication/TableConfig.java
+++ app/src/main/java/com/example/miljac/myapplication/TableConfig.java
@@ -29,10 +29,10 @@ public class TableConfig {
     public static int SOLO_NOTE_LOWER_BOUNDARY = 53;
     public static int SOLO_NOTE_UPPER_BOUNDARY = 80;
 
-    public static int OKO_COLOR = 0xFF2CA5A3;
+    public static int OKO_COLOR = 0xFF5BE1CA;
     public static int GUMB_COLOR = 0xFFD44FB6;
-    public static int DJETELINA_COLOR = 0xFFABFA10;
-    public static int ZVIJEZDA_COLOR = 0xFFFFDE3E;
+    public static int DJETELINA_COLOR = 0xFFA4F80F;
+    public static int ZVIJEZDA_COLOR = 0xFFED9D1D;
 
 
 
