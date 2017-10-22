d484c8422ef64d1a92a0c8ba669bd345e0960100
milicajac <miljenko.stojicic@gmail.com>
2017-10-22 23:03:24 +0200

    zamjena slikica i jos malo oko gui

--- app/src/main/java/com/example/miljac/myapplication/MainActivity.java
+++ app/src/main/java/com/example/miljac/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.miljac.myapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.TextureView;
 import android.view.View;
 import android.view.Menu;
@@ -36,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         itemOko.setData("OKO", "Onaj koji vidi sve,pa i tebe", R.drawable.pin39);
         itemGumb.setData("GUMB", "Statican, ali pouzdan", R.drawable.pin40);
@@ -68,6 +71,46 @@ public class MainActivity extends AppCompatActivity {
         spinnerPlayer1.setAdapter(new MyAdapter(this, R.layout.row, getAllList()));
         spinnerPlayer1.setBackgroundColor(Color.TRANSPARENT);
         spinnerPlayer1.setDrawingCacheBackgroundColor(Color.TRANSPARENT);
+
+
+
+        spinnerPlayer1.setOnTouchListener(
+
+
+                (new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent m) {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)// && hasFocus)
+                {
+                    getWindow().getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                }
+                return false;
+            }
+        }));
+
+        spinnerPlayer1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)// && hasFocus)
+                {
+                    getWindow().getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                }
+            }
+        });
 
         spinnerPlayer2 = (Spinner) findViewById(R.id.spinner_player2);
         final MyAdapter adapterSpinner2 = new MyAdapter(this, R.layout.row, new ArrayList<ListItem>());
@@ -116,23 +159,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         System.out.println("        get    mrm_PLAYER_2_IMG  " + mPrefs.getInt("mrm_PLAYER_2_IMG", 0));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -228,6 +254,19 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("        get    mrm_PLAYER_1_IMG  " + mPrefs.getInt("mrm_PLAYER_1_IMG", 0));
         System.out.println("        get    mrm_PLAYER_2_IMG  " + mPrefs.getInt("mrm_PLAYER_2_IMG", 0));*/
 
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)// && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+
     }
 
 
@@ -278,5 +317,40 @@ public class MainActivity extends AppCompatActivity {
         return allList;
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 
+    public void exit(View view) {
+        finish();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)// && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+    }
 }
--- app/src/main/java/com/example/miljac/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/miljac/myapplication/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.example.miljac.myapplication;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import java.util.concurrent.locks.LockSupport;
 
@@ -11,14 +14,78 @@ import java.util.concurrent.locks.LockSupport;
  */
 
 public class SplashActivity extends AppCompatActivity {
+    private int currentApiVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LockSupport.parkNanos(2_000_000_000);
+        currentApiVersion = android.os.Build.VERSION.SDK_INT;
+        setContentView(R.layout.splash_layout);
+
+
+        /*LockSupport.parkNanos(2_000_000_000);
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();*/
+    }
+
+    public void enter(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        //finish();
+    }
+
+    public void exit(View view) {
         finish();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT)// && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT)// && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT)// && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+
     }
 }
